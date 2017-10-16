4104747dc22d884a08e64f4a9be51b6503da174a
krlim417 <krlim417@gmail.com>
2017-10-16 16:15:41 -0700

    Refactor Dancer class and its subclass BlinkyDancer from functional inheritance to pseudoclassical.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,23 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  this.timeBetweenSteps = timeBetweenSteps;
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.oldStep = makeDancer.step;
 
-  var oldStep = blinkyDancer.step;
+};
 
-  blinkyDancer.step = function() {
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+makeBlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+  //console.log('oldstep', this.oldStep);
+  makeDancer.prototype.step.call(this, this.timeBetweenSteps);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+  console.log('this', this.$node);
+  this.$node.toggle();
 };
